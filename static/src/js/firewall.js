0f0a431224029970e49a38e27502db07c34aa24a
wanghaipeng28 <wanghaipeng28@163.com>
2016-09-29 16:25:29 +0800

    更新回车键提交表单

--- static/src/js/firewall.js
+++ static/src/js/firewall.js
@@ -70,6 +70,16 @@ openerp.ccb_firewall=function(instance){
                     }
                 }
             );
+            //按回车键提交表单
+            $help.find("form").keydown(function(e){
+                if(e.keyCode==13){
+                    $help.find('input').trigger("blur");
+                    var tem=$help.find("form").serialize();
+                    if(o.source_ip && o.dst_ip){
+                        $.post("/ccb_firewall/needs/objects/", tem, doResponse,"json");
+                    }
+                }
+            });
             //利用正则表达式校验ip地址
             function vliIpv4(ip){
                 var reg=/^(2[5][0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})$/;
